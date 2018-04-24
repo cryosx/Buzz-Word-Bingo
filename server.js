29a232d0991f6d5b79cc4855969aed41a59773a6
cryosx <cyruswu.email@gmail.com>
2018-04-24 13:48:45 -1000

    index.html

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
+
+let buzzwords = [];
+
+app.use(express.static('public'));
+
+app.post('/buzzword', (req, res) => {
+  console.log('test');
+  res.send('test');
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
