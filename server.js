5b57321c419d63e6e41e59772adb8940d854a9ca
cryosx <cyruswu.email@gmail.com>
2018-04-24 14:47:10 -1000

    implemented post

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-let buzzwords = [];
+let buzzwords = {};
 
 app.use(express.static('public'));
 
-app.post('/buzzword', (req, res) => {
-  console.log('test');
-  res.send('test');
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/buzzwords', (req, res) => {
+  res.json(buzzwords);
+});
+app.post('/buzzwords', (req, res) => {
+  if (!req.body.buzzWord || !req.body.points) {
+    res.json({ success: false });
+  } else if (
+    typeof req.body.buzzWord !== 'string' ||
+    Number.isNaN(Number.parseInt(req.body.points)) ||
+    typeof Number.parseInt(req.body.points) !== 'number'
+  ) {
+    res.json({ success: false });
+  } else if (buzzwords[`${req.body.buzzWord}`]) {
+    res.json({ success: false });
+  } else {
+    buzzwords[`${req.body.buzzWord}`] = Number.parseInt(req.body.points);
+    res.json({ success: true });
+  }
+});
+app.put('/buzzwords', (req, res) => {});
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
